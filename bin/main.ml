9a14b56f974f3d7a8c7c25c8deaac6fd55b8e3b0
Daniel Patterson <dbp@dbpmail.net>
2025-01-03 11:23:27 -0500

    add better errors when missing fields

--- bin/main.ml
+++ bin/main.ml
@@ -27,29 +27,32 @@ let () =
       (fun request ->
         let%lwt body = Dream.body request in
 
-        let m =
-          body
-          |> Yojson.Safe.from_string
-          |> meta_of_yojson
-        in
-
-        if m.key = key then
-          begin
-            Dream.log "Storing metadata for %s" m.email;
-            match%lwt
-              Dream.sql request ([%rapper
-                                  execute
-                                    {sql|
+        match try body
+              |> Yojson.Safe.from_string
+              |> meta_of_yojson
+              |> Option.some
+          with _ -> None with
+        | None ->
+          Dream.log "Error: Missing fields";
+          Dream.json "ERROR"
+        | Some m ->
+          if m.key = key then
+            begin
+              Dream.log "Storing metadata for %s" m.email;
+              match%lwt
+                Dream.sql request ([%rapper
+                                    execute
+                                      {sql|
                                        INSERT INTO meta (email, assignment, time, content, results, other) VALUES (%string{email}, %string{assignment}, %string{time}, %string{content}, %string{results}, %string{other})
       |sql} record_in] m) with
-            | Ok _ -> Dream.json "OK"
-            | Error err ->
-              Dream.log "Error %s" (Caqti_error.show err);
+              | Ok _ -> Dream.json "OK"
+              | Error err ->
+                Dream.log "Error %s" (Caqti_error.show err);
+                Dream.json "ERROR"
+            end
+          else
+            begin
+              Dream.log "Error: Not authenticated";
               Dream.json "ERROR"
-          end
-        else
-          begin
-            Dream.log "Error: Not authenticated";
-            Dream.json "ERROR"
-          end)
+            end)
   ]
